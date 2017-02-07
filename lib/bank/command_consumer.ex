930a994b0ac1524d198f0dcacbef390010ef77c1
Yin Weijun <gw9812@gmail.com>
2017-02-07 15:57:50 +0800

    handle all three differnt commands with procedue codes

--- lib/bank/command_consumer.ex
+++ lib/bank/command_consumer.ex
@@ -1,6 +1,8 @@
 defmodule Bank.CommandConsumer do
   use GenStage
 
+  alias Bank.Commands.{CreateAccount, DepositMoney, WithdrawMoney}
+
   def start_link(consumer_id) do
   	name = via_tuple(consumer_id)
   	GenStage.start_link(__MODULE__, :ok, name: name)
@@ -16,10 +18,24 @@ defmodule Bank.CommandConsumer do
   	{:consumer, :ok, subscribe_to: [via_tuple(Bank.CommandProducer)]}
   end
 
-  def handle_events(events, _from, state) do
-  	for event <- events do
-  	  IO.inspect {self(), event}
-  	end
+  def handle_events(events, from, state) do
+    for event <- events do
+      handle_event(event)
+    end
+
   	{:noreply, [], state}
   end
+
+  defp handle_event(%CreateAccount{}) do
+  	IO.puts "just create a new account"
+  end
+
+  defp handle_event(%DepositMoney{amount: amount} = event) do
+  	IO.puts "deposit money: #{amount}"
+  end
+
+  defp handle_event(%WithdrawMoney{amount: amount}) do
+  	IO.puts "withdraw money: #{amount}"
+  end
+
 end
